7da3620a2b96cf9e56c8dfa8046a52599b659af3
optimax <optimax@kepler.local>
2021-08-08 12:41:24 +0400

    added infra

---
+++ docker-monolith/infra/terraform/main.tf
@@ -0,0 +1,35 @@
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone_id
+}
+
+resource "yandex_compute_instance" "app" {
+  name = "docker-${count.index}"
+  count = var.instances
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+
+  resources {
+    cores         = 2
+    memory        = 4
+    core_fraction = 100
+  }
+
+  boot_disk {
+    initialize_params {
+      # Указать id образа созданного в предыдущем домашем задании
+      image_id = var.image_id
+      size = 20
+    }
+  }
+
+  network_interface {
+    # Указан id подсети default-ru-central1-a
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+}
